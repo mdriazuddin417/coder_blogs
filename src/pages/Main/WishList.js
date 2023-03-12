62f66c3fe2cdd4ab95ebed1f3f8ec9158cf47ac0
mdriazuddin417 <mdriazuddin417@gmail.com>
2023-03-12 07:02:45 +0600

    added contact page

--- src/pages/Main/WishList.js
+++ src/pages/Main/WishList.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import SingleBlog from "../../components/SingleBlog";
 
 const WishList = () => {
-  return <div>WishList</div>;
+  const blogs = useSelector((state) => state.blogs.blogs);
+  return (
+    <div className="py-10 grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
+      {blogs
+        .filter((blog) => blog.status)
+        .map((item, index) => (
+          <SingleBlog key={index} item={item} />
+        ))}
+    </div>
+  );
 };
 
 export default WishList;
